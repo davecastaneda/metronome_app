766d5f23feb0a942b320fe1fda46ce0450f39d92
davecastaneda <webdeveloperdave@gmail.com>
2023-03-01 18:27:53 -0600

    Added helper functions

--- app.js
+++ app.js
@@ -5,26 +5,46 @@ const increaseTempoBtn = document.querySelector('.increase-tempo');
 const tempoSlider = document.querySelector('.slider');
 const startStopBtn = document.querySelector('.start-stop');
 const subtractBeats = document.querySelector('.subtract-beats');
-const addBeats = document.querySelector('add-beats');
-const measureCount = document.querySelector('measure-count');
+const addBeats = document.querySelector('.add-beats');
+const measureCount = document.querySelector('.measure-count');
 
 let bpm = 140;
 let beatsPerMeasure = 4;
+let tempoTextString = 'Hot Dawg!';
 
 decreaseTempoBtn.addEventListener('click', () => {
     if (bpm <= 20) { return };
     bpm--;
-    tempoDisplay.textContent = bpm;
-    tempoSlider.value = bpm;
+    validateTempo();
+    updateMetronome();
 });
 increaseTempoBtn.addEventListener('click', () => {
     if (bpm >= 280) { return };
     bpm++;
-    tempoDisplay.textContent = bpm;
-    tempoSlider.value = bpm;
+    validateTempo();
+    updateMetronome();
 });
 tempoSlider.addEventListener('input', () => {
     bpm = tempoSlider.value;
+    validateTempo();
+    updateMetronome();
+});
+subtractBeats.addEventListener('click', () => {
+    if (beatsPerMeasure <= 1) { return };
+    beatsPerMeasure--;
+    measureCount.textContent = beatsPerMeasure;
+});
+addBeats.addEventListener('click', () => {
+    if (beatsPerMeasure >= 24) { return };
+    beatsPerMeasure++;
+    measureCount.textContent = beatsPerMeasure;
+});
+
+function updateMetronome() {
     tempoDisplay.textContent = bpm;
     tempoSlider.value = bpm;
-});
+}
+function validateTempo() {
+    if (bpm <= 20) { return };
+    if (bpm >= 280) { return };
+}
