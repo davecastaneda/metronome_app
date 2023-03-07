71d27379ae6d89701f6227c1641caecb24a67f4e
davecastaneda <webdeveloperdave@gmail.com>
2023-03-07 10:25:58 -0600

    Added the start timer method

--- tempo_clock.js
+++ tempo_clock.js
@@ -1,3 +1,16 @@
+// Timer Constructor Function
 function Timer(callback, timeInterval, options) {
     this.timeInterval = timeInterval;
+
+    // Method to start timer
+    this.start = () => {
+        // Sets expected time - this is  when we start the timer, plus whatever the time interval is
+        this.expected = Date.now() + this.timeInterval;
+        // Starts the timeout and saves the ID in a property, so we can cancel it later
+        this.theTimeout = null;
+
+        if (options.immediate) {
+            callback();
+        }
+    }
 }
