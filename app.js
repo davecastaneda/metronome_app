89c8d3fdd6316c6a8972bcebb8c93085af0967a7
davecastaneda <webdeveloperdave@gmail.com>
2023-03-12 23:21:56 -0500

    Debugged timing text issue in update Metronome

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ function updateMetronome() {
 
     if (bpm <= 40) { tempoTextString = "Very, Very Slow" };
     if (bpm > 40 && bpm < 80) { tempoTextString = "Very Slow" };
-    if (bpm > 80 && bpm < 120) { tempoTextString = "Slow" };
+    if (bpm > 80 && bpm < 139) { tempoTextString = "Slow" };
     if (bpm = 140) { tempoTextString = "Medium" };
-    if (bpm > 120 && bpm < 180) { tempoTextString = "Moderate" };
+    if (bpm >= 141 && bpm < 180) { tempoTextString = "Moderate" };
     if (bpm > 180 && bpm < 220) { tempoTextString = "Moderately Fast" };
     if (bpm > 220 && bpm < 240) { tempoTextString = "Fast" };
     if (bpm > 240 && bpm < 260) { tempoTextString = "VERY Fast" };
