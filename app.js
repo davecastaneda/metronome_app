39e07f79b2fc73c8279f3e89939b5aacdda2ac5b
davecastaneda <webdeveloperdave@gmail.com>
2023-03-12 23:14:19 -0500

    Tempo setting updated

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ function updateMetronome() {
     if (bpm <= 40) { tempoTextString = "Very, Very Slow" };
     if (bpm > 40 && bpm < 80) { tempoTextString = "Very Slow" };
     if (bpm > 80 && bpm < 120) { tempoTextString = "Slow" };
+    if (bpm = 140) { tempoTextString = "Medium" };
     if (bpm > 120 && bpm < 180) { tempoTextString = "Moderate" };
     if (bpm > 180 && bpm < 220) { tempoTextString = "Moderately Fast" };
     if (bpm > 220 && bpm < 240) { tempoTextString = "Fast" };
